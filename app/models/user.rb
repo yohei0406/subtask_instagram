3db90ec2f53e3559ca66885909af46b223c2851d
yohei <yohei.19930406@gmail.com>
2020-10-05 09:52:06 +0900

    add username and fullname for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
+  validates :email, uniqueness: true
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+  									sessions:      'users/sessions',
+                 					passwords:     'users/passwords',
+        							registrations: 'users/registrations'
+        						  }
 end
